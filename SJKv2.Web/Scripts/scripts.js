ba1b69e6675f580a7a3ffedf28c7b5047f392060
Andy Wybenga <awybenga@katlyn.local>
2018-05-11 15:22:03 -0400

    Add project files.

---
+++ SJKv2.Web/Scripts/scripts.js
@@ -0,0 +1,27 @@
+﻿/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function navResponse() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "navigation") {
+        x.className += " responsive";
+    } else {
+        x.className = "navigation";
+    }
+}
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("backToTop").style.display = "block";
+    } else {
+        document.getElementById("backToTop").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
